38d4a1b2b71457707484544356c094fcc0e96730
Florian Weber <f.weber@ppm-vi.de>
2023-12-26 13:44:33 +0100

    chore: further optimize day5

--- day5/index.ts
+++ day5/index.ts
@@ -1,40 +1,32 @@
 export function part1(input: string) {
   const lines = input.trim().split("\n");
   const seeds = lines[0].split(":")[1].trim().split(" ").map(Number);
-  const maps: number[][][] = [];
-  let cnt = 0;
-
-  for (let i = 2; i < lines.length; i++) {
-    const line = lines[i];
-
-    if (line === "") {
-      cnt++;
-    } else {
-      if (!Array.isArray(maps[cnt])) maps[cnt] = [];
-
-      if (!line.includes(":")) {
-        let map = line.split(" ");
-        const destination = parseInt(map[0]);
-        const source = parseInt(map[1]);
-        const range = parseInt(map[2]);
-
-        maps[cnt].push([source, source + range, destination]);
-      }
-    }
-  }
-
   let location: number | undefined;
+  let cnt = 0;
 
   for (const seed of seeds) {
     let lastFound = seed;
+    let hasFound = false;
 
-    for (const map of maps) {
-      for (const ranges of map) {
-        const [sourceStart, sourceEnd, destinationStart] = ranges;
+    for (let i = 2; i < lines.length; i++) {
+      const line = lines[i];
 
-        if (lastFound >= sourceStart && lastFound <= sourceEnd) {
-          lastFound = destinationStart + (lastFound - sourceStart);
-          break;
+      if (line === "") {
+        cnt++;
+        hasFound = false;
+      } else if (!hasFound) {
+        if (!line.includes(":")) {
+          let map = line.split(" ");
+
+          const range = parseInt(map[2]);
+          const destinationStart = parseInt(map[0]);
+          const sourceStart = parseInt(map[1]);
+          const sourceEnd = sourceStart + range;
+
+          if (lastFound >= sourceStart && lastFound <= sourceEnd) {
+            lastFound = destinationStart + (lastFound - sourceStart);
+            hasFound = true;
+          }
         }
       }
     }
